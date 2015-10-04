ea9d4deae830f9dff6c2d9edf1fd0590d285a743
Devan Patel <devanp92@ufl.edu>
2015-10-04 10:15:33 -0400

    Renaming project to ghosttag in setup.py

--- setup.py
+++ setup.py
@@ -2,16 +2,16 @@
 
 import os
 from setuptools import setup
-from ghost_tag import _version
+from ghosttag import _version
 
 
 setup(name="ghosttag",
       version=_version.__version__,
       description="Tag updater for Ghost",
-      long_description=open("README.me").read(),
+      long_description=open("README.md").read(),
       author="Devan Patel",
       author_email="devanppatel92@gmail.com",
-      url="https://github.com/devanp92/ghost-tag-updater",
+      url="https://github.com/devanp92/ghost-tag",
       license="MIT",
       packages=["ghosttag"],
       entry_points={"console_scripts": ["ghosttag = ghosttag.ghosttag:main"]},
